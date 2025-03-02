915e4e3ae8a47f40f05bc1c64c1e221147da40d0
dianka_mikhina_04@vk.com <dianka_mikhina_04@vk.com>
2025-03-02 21:59:05 +0500

    делаю

---
+++ Task_13_02/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_13_02
+{
+    class Pet
+    {
+        // Свойства класса
+        public string Name { get; set; }
+        public string AnimalType { get; set; }
+        public int Age { get; set; }
+        public double Weight { get; set; }
+        public bool IsHealthy { get; set; } // true - здоров, false - нездоров
+
+        
+        // Конструктор с параметрами
+        public Pet(string name, string animalType, int age, double weight, bool isHealthy)
+        {
+            Name = name;
+            AnimalType = animalType;
+            Age = age;
+            Weight = weight;
+            IsHealthy = isHealthy;
+        }
+        // Конструктор по умолчанию
+        public Pet()
+        {
+            Name = "Бобик";
+            AnimalType = "Собака";
+            Age = 3;
+            Weight = 10.5;
+            IsHealthy = true;
+        }
+
+        // Метод для вывода информации о питомце
+        public void DisplayInfo()
+        {
+            string healthStatus = IsHealthy ? "Здоров" : "Нездоров";
+            Console.WriteLine($"Имя: {Name}, Вид: {AnimalType}, Возраст: {Age} года, Вес: {Weight} кг, Состояние здоровья: {healthStatus}");
+        }
+
+        // Метод для изменения веса животного
+        public void ChangeWeight(double newWeight)
+        {
+            Weight = newWeight;
+            Console.WriteLine($"Вес питомца {Name} изменен на {Weight} кг.");
+        }
+
+        // Метод для изменения состояния здоровья
+        public void ChangeHealthStatus(bool healthStatus)
+        {
+            IsHealthy = healthStatus;
+            Console.WriteLine($"Состояние здоровья питомца {Name} изменено на {(IsHealthy ? "Здоров" : "Нездоров")}");
+        }
+    }
+
+}
+
---
+++ Task_13_02/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task_13_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Создание объекта с помощью конструктора с параметрами
+            Pet pet2 = new Pet("Бобик", "Собака", 3, 10.5, true);
+            pet2.DisplayInfo();
+
+            // Изменение веса и состояния здоровья
+            pet2.ChangeWeight(11.0);
+            pet2.ChangeHealthStatus(false);
+            pet2.DisplayInfo();
+        }
+    }
+}
